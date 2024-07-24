1a36dd2e114d41ae93cc0fed8ad7eac66222e2e5
ASUS <ASUS@A>
2024-07-25 02:00:54 +0700

    Add project files.

---
+++ AMD-201Assignment/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AMD_201Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AMD_201Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AMD-201Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AMD_201Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
